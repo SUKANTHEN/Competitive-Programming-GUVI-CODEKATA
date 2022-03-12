f90c06957928511cdf47cbd369b59111de1f5e6f
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-03-12 06:53:04 +0530

    Update Write a code to get the input in the given format and print the output in the given format.py

--- input_output/Write a code to get the input in the given format and print the output in the given format.py
+++ input_output/Write a code to get the input in the given format and print the output in the given format.py
@@ -1,1 +1,10 @@
+# Input Description: To take an integer value
 
+#Output Description: Print the integer value
+
+#Sample Input : 2
+#Sample Output : 2
+
+# SOLUTION
+userInput = int(input())
+print(userInput)
