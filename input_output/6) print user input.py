c1d7d11813ed32b8a75bddc3f2337e30bfc060d7
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-03-12 07:38:06 +0530

    Create 6) print user input.py

---
+++ input_output/6) print user input.py
@@ -0,0 +1,13 @@
+# Sample Input :
+# 2 5
+# 2 5 6
+# 2 4 5
+# Sample Output :
+# 2 5
+# 2 5 6
+# 2 4 5
+
+# SOLUTION:
+print(*[int(x) for x in input().split()])
+print(*[int(x) for x in input().split()])
+print(*[int(x) for x in input().split()])
