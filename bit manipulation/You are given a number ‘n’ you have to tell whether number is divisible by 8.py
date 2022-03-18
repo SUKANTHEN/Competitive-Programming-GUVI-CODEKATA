ba37c6721e0ebcaf9860eee6b3923317d063cc4e
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-03-18 16:00:59 +0530

    Create You are given a number ‘n’ you have to tell whether number is divisible by 8.py

---
+++ bit manipulation/You are given a number ‘n’ you have to tell whether number is divisible by 8.py
@@ -0,0 +1,13 @@
+# Q) You are given a number ‘n’ you have to tell whether number is divisible by 8. Print 1 if it is divisible and 0 if it is not.
+# Sample Input :
+# 16
+# Sample Output :
+# 1
+'''
+SOLUTION:
+'''
+n = int(input())
+if n%8 == 0:
+    print(1)
+else:
+    print(0)
