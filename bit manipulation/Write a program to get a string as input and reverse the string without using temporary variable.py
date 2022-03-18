754aae19162159052926fee54b905f93df4b4dc3
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-03-18 16:09:06 +0530

    Create Write a program to get a string as input and reverse the string without using temporary variable.py

---
+++ bit manipulation/Write a program to get a string as input and reverse the string without using temporary variable.py
@@ -0,0 +1,11 @@
+# Q) Write a program to get a string as input and reverse the string without using temporary variable.
+# Sample Input :
+# GUVI
+# Sample Output :
+# IVUG
+'''
+SOLUTION:
+'''
+string = input()
+# reversed() function is used to reverse a string
+print("".join(reversed(string)))
