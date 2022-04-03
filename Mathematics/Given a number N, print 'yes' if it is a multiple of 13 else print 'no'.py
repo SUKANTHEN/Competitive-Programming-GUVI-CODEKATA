cb1a7525bf6b99fb98c1c2e6ad46ba3704bc7672
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-04-03 16:16:36 +0530

    Create Given a number N, print 'yes' if it is a multiple of 13 else print 'no'.py

---
+++ Mathematics/Given a number N, print 'yes' if it is a multiple of 13 else print 'no'.py
@@ -0,0 +1,13 @@
+# Q) Given a number N, print 'yes' if it is a multiple of 13 else print 'no'.
+# INPUT
+# 26
+# OUTPUT
+# yes
+"""
+SOLUTION:
+"""
+n = int(input())
+if n % 13 == 0:
+    print("yes")
+else:
+    print("no")
