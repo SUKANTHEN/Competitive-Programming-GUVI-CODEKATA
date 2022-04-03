19c47c839195ff7b22963980cb091d5960e2bbef
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-04-03 16:14:10 +0530

    Create Find the minimum among 10 numbers.py

---
+++ Mathematics/Find the minimum among 10 numbers.py
@@ -0,0 +1,11 @@
+# Q) Find the minimum among 10 numbers.
+# Sample Testcase :
+# INPUT
+# 5 4 3 2 1 7 6 10 8 9
+# OUTPUT
+# 1
+"""
+SOLUTION:
+"""
+n = list(map(int,input().split(" ")))
+print(min(n))
