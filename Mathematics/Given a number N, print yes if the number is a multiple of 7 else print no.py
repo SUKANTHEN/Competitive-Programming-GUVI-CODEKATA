cdd19e0a4ff4dd788bfe5c64260df6a24d95e8e4
SUKANTHEN.S.S <49028941+SUKANTHEN@users.noreply.github.com>
2022-04-03 16:15:03 +0530

    Create Given a number N, print yes if the number is a multiple of 7 else print no.py

---
+++ Mathematics/Given a number N, print yes if the number is a multiple of 7 else print no.py
@@ -0,0 +1,14 @@
+# Q) Given a number N, print yes if the number is a multiple of 7 else print no.
+# Sample Testcase :
+# INPUT
+# 49
+# OUTPUT
+# yes
+"""
+SOLUTION:
+"""
+n = int(input())
+if n%7 ==0:
+    print("yes")
+else:
+    print("no")
